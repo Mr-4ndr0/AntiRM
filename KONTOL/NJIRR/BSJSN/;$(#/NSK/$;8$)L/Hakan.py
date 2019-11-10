299d89e696b2664e6ee0eaf327e3d6cbb4a0de09
Muhamad Royyani Ganteng Sejagad:V <55822959+MrBarBar05@users.noreply.github.com>
2019-11-10 23:33:50 +0700

    Create Hakan.py

---
+++ KONTOL/NJIRR/BSJSN/;$(#/NSK/$;8$)L/Hakan.py
@@ -0,0 +1,4 @@
+print "Ehh Anjing Kok Udh Sampe Sini Mau Ngapain?"
+print "Mau Recode Yaa:v Coba Lah Belajar Bikin Sendiri"
+print "Jangan Recode Mulu:v"
+print "Recode Tulang Coli Tapi Colinya Pake Balsem:v"
